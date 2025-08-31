8cb8c47f7bc5b6878f9fc9de64dfc2e132cafb58
Irshad <ih@bluedock.dk>
2025-08-31 16:52:54 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
-const heading = React.createElement(
-    "h1", 
-    {id:"heading", xyz:"abcd"}, 
-    "Hello world from React!");
+import ReactDOM from "react-dom/client";
 
-console.log(heading)    ;
+// React.createElement => Object => HTMLElement(render)
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abcd" },
+  "Hello world from React!"
+);
+
+console.log(heading);
+
+// JSX => React.createElement => ReactElement-JS Object => HTMLElement
+const jsxHeading = <h1 id="heading">Namste React using JSX</h1>;
+
+console.log(jsxHeading);
+
+const spanElement = <span>this is span</span>
+
+const Title = () => <h1>{spanElement} Namste React Title Component</h1>;
+
+const HeadingComponent = () => (
+  <div className="container">
+    <Title />
+    <Title></Title>
+    {Title()}
+    {jsxHeading}
+    <h1 className="heading">Namste React Heading Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<HeadingComponent />);
